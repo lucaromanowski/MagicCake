17973ad1de59ce2d6448aa155f9613b38b6352e4
lucaromanowski <lucaromanowski@wp.pl>
2017-09-08 11:54:09 +0200

    File loader
    in progress (now it can load files form hdd an convert them to regular objects.

--- src/resources/projectloader.py
+++ src/resources/projectloader.py
@@ -5,7 +5,9 @@ Loading project
 import os
 import pickle
 
+from .project import NewProject
 from settings import *
+from sprites import Cake, Ingredient
 
 
 class ProjectLoader(object):
@@ -15,7 +17,7 @@ class ProjectLoader(object):
 
 	def __init__(self, program):
 		self.program = program
-		self.all_projects = []
+		self.all_temp_loaded_projects = []
 		print('ProjectLoader Created')
 
 
@@ -26,8 +28,13 @@ class ProjectLoader(object):
 		by passing the path argument. If not, method will use default path.
 		'''
 		print('load_all_projects ivoked')
+		# Check if there is any given path, if not use default path
 		if not path:
 			path = os.path.join(os.path.dirname(os.path.abspath(__file__)), SAVE_FOLDER)
+
+		# Reset all projects list
+		self.reset()
+		
 		print('path with pos path join: ', str(path))
 
 		# Get project folders
@@ -51,9 +58,89 @@ class ProjectLoader(object):
 							# Load file with pickle
 							proj = pickle.load(f) 
 							# test append
-							self.all_projects.append(proj)
+							self.all_temp_loaded_projects.append(proj)
 				except:
 					print("Can't load save file")
 
-		print('All loaded projects: ', str(self.all_projects))
+		print('All loaded projects: ', str(self.all_temp_loaded_projects))
 		print()
+
+		# Converting collection into proper project objects
+		self.convert_collection(self.all_temp_loaded_projects)
+
+
+	def reset(self):
+		'''
+		This method makes list of loaded projects empty.
+		'''
+		self.all_temp_loaded_projects = []
+
+
+	def convert_collection(self, collection):
+		'''
+		This method converts temp projects collection into projects. It converts temp cakes
+		into cakes and temp ingredients into ingredients.
+		'''
+		print()
+		print('Converting collection form temp to regular project')
+		print('Given collection: ', str(collection))
+
+		print('Goingt through the collection')
+
+		# List of final projects 
+		projects_list = []
+		# Recreating projects from temporary projects 
+		for project in collection:
+			print('Project: ', str(project))
+			print('Project name: ', str(project.name))
+			# Creating new project
+			p = NewProject(project.name)
+			print('Project created: ', str(p.name))
+			print()
+			print('Going through tempAll in  temp project')
+			for TempAll in project.project_cakes:
+				print(str(TempAll))
+				print('Temp cake: ', str(TempAll.cake.name), ', ingredients: ' ,str(TempAll.ingredients))
+				print('Creating cake')
+				# Create cake object
+				c = Cake(self.program, TempAll.cake.name)
+				print('Cake created: ', str(c.name))
+				print()
+				print('Creating ingredients')
+				# Create ingredient object
+				for ingredient in TempAll.ingredients:
+					i = Ingredient(self.program, 
+								   ingredient.name,
+								   ingredient.x,
+								   ingredient.y,
+								   ingredient.how_much,
+								   ingredient.protein,
+								   ingredient.carb,
+								   ingredient.fat,
+								   ingredient.kcal,
+								   ingredient.package_size,
+								   ingredient.price)
+					print('Ingredient created: ', str(i.name))
+					# Adding ingredient to cake
+					c.ingredients.add(i)
+					print('Ingredient added')
+				# Appending Cake to project
+				p.cakes.append(c)
+				print('Cake appended to project')
+			#Appending project to projects list
+			projects_list.append(p)
+			print('Project appended to projects list')
+			print('Projects list: ', str(projects_list))
+			print('End convert collection')
+			print()
+		return projects_list
+
+
+
+
+
+
+
+
+
+		
